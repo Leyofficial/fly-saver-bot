fe942ed8f58352f01377e05084344f43cc3f0460
Ley official <danyilfreelance@gmail.com>
2024-08-04 01:04:33 +0100

    feat 36594086: fixed correct query request

--- requests_to_api/get_flight_info.py
+++ requests_to_api/get_flight_info.py
@@ -1,13 +1,19 @@
 import requests
 
+from requests_to_api.get_airports_info import get_all_airports
+
 
 def get_flight_one_way(data):
-    adults, fromId, toId, departDate, adults = data
+    fromId, toId, departDate, adults = data
     url = "https://skyscanner80.p.rapidapi.com/api/v1/flights/search-one-way"
 
-    querystring = {"fromId": {fromId},
-                   "toId": {toId}, "departDate": {departDate},
-                   "adults": {adults}, "cabinClass": "economy"}
+    querystring = {
+        "fromId": fromId,
+        "toId": toId,
+        "departDate": departDate,
+        "adults": adults,
+        "cabinClass": "economy"
+    }
 
     headers = {
         "x-rapidapi-key": "1965246244msh0797ea4cda2a707p1482eajsn816d9c9d674a",
@@ -16,18 +22,24 @@ def get_flight_one_way(data):
 
     response = requests.get(url, headers=headers, params=querystring)
 
-    print(response.json())
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
 
 
 def get_flight_roundtrip(data):
-    adults, fromId, toId, departDate, returnDate, adults = data
-    import requests
-
+    fromId, toId, departDate, returnDate, adults = data
     url = "https://skyscanner80.p.rapidapi.com/api/v1/flights/search-roundtrip"
 
-    querystring = {"fromId": {fromId},
-                   "toId": {toId},
-                   "departDate": {departDate}, "returnDate": {returnDate}, "adults": {adults}, "cabinClass": "economy"}
+    querystring = {
+        "fromId": fromId,
+        "toId": toId,
+        "departDate": departDate,
+        "returnDate": returnDate,
+        "adults": adults,
+        "cabinClass": "economy"
+    }
 
     headers = {
         "x-rapidapi-key": "1965246244msh0797ea4cda2a707p1482eajsn816d9c9d674a",
@@ -36,4 +48,29 @@ def get_flight_roundtrip(data):
 
     response = requests.get(url, headers=headers, params=querystring)
 
-    print(response.json())
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def get_summary_results(data):
+    departure, arrival, departure_date, arrival_date, trip_type, adults = data
+    fromId = get_all_airports(departure)
+    toId = get_all_airports(arrival)
+    departDate = departure_date
+    returnDate = arrival_date
+
+    try:
+        if trip_type == 'one_way':
+            result = get_flight_one_way([fromId, toId, departDate, adults])
+        elif trip_type == 'return_way':
+            result = get_flight_roundtrip([fromId, toId, departDate, returnDate, adults])
+        else:
+            raise ValueError("Invalid trip type")
+
+        print(result)
+        return result
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
